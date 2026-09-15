fa3339ab1ce73620cec6f23ec8133c19bbf3d6f6
reizzaoprogramador <reizao1977@gmail.com>
2026-09-15 16:21:18 -0300

    fish: configuracao orignal

--- config.fish
+++ config.fish
@@ -3,15 +3,14 @@
 # @mission: configuracao + inicializacao + padronizacao de ambiente
 
 if status is-interactive
-    # desativar a mensagem de boas-vindas (greeting) do Fish Shell sempre que você abre um novo terminal.
-    set -g fish_greeting 
+    set -g fish_greeting # desativar a mensagem de boas-vindas (greeting) do Fish Shell sempre que você abre um novo terminal.
+    loadEnvs
 end
-
 
 # ==============================================================================
 # @README_FILE
 #
 # @IMPORTANTE_PROFILE:
-# - Arquivo redefinido para o estado padrao de fabrica.
-# - As configuracoes nativas do Fish sao carregadas diretamente pelo binario.
+# - Carrega a funcao loadEnvs na inicializacao interativa.
+# - As variaveis de $HOME/dotgit/envs/.env_shells ficam prontas para uso.
 # ==============================================================================
